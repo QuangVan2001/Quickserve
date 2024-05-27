db6b3acab647405ac5118b30f5be76e8459e5851
HOANG Trung Thong <thoang5@mantu.com>
2024-05-27 22:04:25 +0700

    test

--- Src/Presentation/QuickServe.WebApi/Program.cs
+++ Src/Presentation/QuickServe.WebApi/Program.cs
@@ -26,14 +26,14 @@ using QuickServe.Infrastructure.FileManager.Contexts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddApplicationLayer();
-//builder.Services.AddPersistenceInfrastructure(builder.Configuration);
-//builder.Services.AddFileManagerInfrastructure(builder.Configuration);
-//builder.Services.AddIdentityInfrastructure(builder.Configuration);
-//builder.Services.AddResourcesInfrastructure();
+builder.Services.AddApplicationLayer();
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+builder.Services.AddFileManagerInfrastructure(builder.Configuration);
+builder.Services.AddIdentityInfrastructure(builder.Configuration);
+builder.Services.AddResourcesInfrastructure();
 
 builder.Services.AddScoped<IAuthenticatedUserService, AuthenticatedUserService>();
-//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddDistributedMemoryCache();
 //builder.Services.AddJwt(builder.Configuration);
 
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -56,7 +56,7 @@ builder.Services.AddCors(x =>
 
     });
 });
-//builder.Services.AddCustomLocalization(builder.Configuration);
+builder.Services.AddCustomLocalization(builder.Configuration);
 
 //builder.Services.AddHealthChecks();
 builder.Services.AddScoped<IAuthenticatedUserService, AuthenticatedUserService>();
@@ -64,19 +64,19 @@ builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Confi
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope())
-//{
-//    var services = scope.ServiceProvider;
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
 
-//    await services.GetRequiredService<IdentityContext>().Database.MigrateAsync();
-//    await services.GetRequiredService<ApplicationDbContext>().Database.MigrateAsync();
-//    // await services.GetRequiredService<FileManagerDbContext>().Database.MigrateAsync();
+    await services.GetRequiredService<IdentityContext>().Database.MigrateAsync();
+    await services.GetRequiredService<ApplicationDbContext>().Database.MigrateAsync();
+    // await services.GetRequiredService<FileManagerDbContext>().Database.MigrateAsync();
 
-//    //Seed Data
-//    await DefaultRoles.SeedAsync(services.GetRequiredService<RoleManager<ApplicationRole>>());
-//    await DefaultBasicUser.SeedAsync(services.GetRequiredService<UserManager<ApplicationUser>>());
-//    await DefaultData.SeedAsync(services.GetRequiredService<ApplicationDbContext>());
-//}
+    //Seed Data
+    await DefaultRoles.SeedAsync(services.GetRequiredService<RoleManager<ApplicationRole>>());
+    await DefaultBasicUser.SeedAsync(services.GetRequiredService<UserManager<ApplicationUser>>());
+    await DefaultData.SeedAsync(services.GetRequiredService<ApplicationDbContext>());
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -86,7 +86,7 @@ if (app.Environment.IsDevelopment())
 app.UseSwagger();
 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "QuickServe.WebApi v1"));
 
-//app.UseCustomLocalization();
+app.UseCustomLocalization();
 app.UseCors("Any");
 app.UseRouting();
 app.UseHttpsRedirection();
